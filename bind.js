067017c7be59519896cbd76fdb0c85ab011aeabe
yungtiec <tctammychu@gmail.com>
2016-04-20 10:42:23 -0700

    nodeschool partial application with bind module

--- bind.js
+++ bind.js
@@ -17,4 +17,15 @@ MDN def of bind()
 The bind() method creates a new function that, when called, has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
 The key here is 'creates a new function'
 so a new function is defined, not called. 
+
+compared to partial application with apply
+apply() will directly invoke the function
+thats why in the logger.js, its wrapped in a anonymous fn
+
+function logger(namespace) {
+  return function() {
+    args = Array.prototype.slice.call(arguments);
+    console.log.apply(null,[namespace].concat(args))
+  }
+}
 */
